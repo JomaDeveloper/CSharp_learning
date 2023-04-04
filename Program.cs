92b3502a6eace97ebd0ab61353e9b383485e04e3
Kirill <ktutaev@tickeron.com>
2023-04-04 14:56:11 +0300

    [Learning] MathOperations#2

--- Program.cs
+++ Program.cs
@@ -5,45 +5,22 @@ namespace csharp_learning{
     class Program{
         
         static void Main() {
-            //Converting data types
-            Console.Write("Введите число с точкой: ");
-            float user_input = (float)Convert.ToDouble(Console.ReadLine());
-            // float user_input = float.Parse(Console.ReadLine());
+            // Library Math provides many maths operations.
+            // Console.WriteLine(Math.PI);
+            // Console.WriteLine(Math.Abs(-20));
+            // Console.WriteLine(Math.Ceiling(4.11f));
+            // Console.WriteLine(Math.Floor(4.11f));
+            // Console.WriteLine(Math.Round(4.11f));
+            // Console.WriteLine(Math.Min(5, 0));
+            // Console.WriteLine(Math.Max(5, 0));
+            // Console.WriteLine(Math.Pow(5, 2)); // Степень
 
-            //Maths Operations
-            float result;
-            result = user_input + 10f;
-            // result = user_input - 15f;
-            // result = user_input * 15f;
-            // result = user_input / 5f;
-            // result = user_input % 10f;
-            // result *= 2f
-            // result++ or result --
-
-            Console.WriteLine("Переменная: " + result);
-
+            Console.WriteLine("Введите радиус круга: ");
+            double rad = Convert.ToDouble(Console.ReadLine());
+            double area = Math.PI * Math.Pow(rad, 2);
+            Console.WriteLine("Площадь круга с радиусом {0} равна {1}", rad, Math.Round(area, 2)); // Вывод можно сделать и так.
         }
 
     }
 
 }
-
-/* In C#, an explicit cast is a way to convert a value of one type to another type, 
-but with the possibility of data loss or exceptions. When an explicit cast is used, 
-the compiler requires that the cast be explicitly stated in the code.
-
-An explicit cast is used to convert a value from a larger data type to a smaller one, 
-or from one data type to another that is not implicitly convertible. 
-For example, you might use an explicit cast to convert a double to a float, or an int to a short.
-
-Here's an example of how to use an explicit cast in C#:
-
-double d = 3.14159;
-float f = (float)d; // Explicit cast from double to float
-In this example, the value of the double variable d is converted to a float using an explicit cast. 
-Since a float has a smaller range and precision than a double, some data loss may occur during the conversion.
-
-It's important to note that if the value being cast cannot be represented by the target data type, 
-an exception will be thrown at runtime. For example, if you try to cast a value of 1000000000000 to an int, 
-an OverflowException will be thrown, because the value is too large to fit in an int.
-*/
