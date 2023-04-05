b195c09318aedacd5dee093b0698205354eb581c
Kirill <ktutaev@tickeron.com>
2023-04-05 15:59:41 +0300

    [Learning] Switch-case

--- Program.cs
+++ Program.cs
@@ -5,18 +5,30 @@ namespace csharp_learning{
     class Program{
         
         static void Main() {
-           
-            int a = 3;
-            string word = "hello";
-            bool isHappy = true; 
+           // Switch-case like conditional operator.
+           // But we can only check equality of the variable.
+           // But it can me useful when we expect a lot of inputs that needs variable checking.
+           // We can easily do it via if-else.
+           short user_input = Convert.ToInt16(Console.ReadLine());
 
-            if(a > 7) { 
-                Console.WriteLine("1");
-            } else if(a < 5 && isHappy) // && - and; || - or
-                Console.WriteLine("2"); // Если после условия 1 строчка кода, то можно не ставить {}.
-            else {
-                Console.WriteLine("3");
-            }
+           switch(user_input) {
+                case 5: //Code until a must-have break;
+                    Console.WriteLine("Number is 5"); 
+                    break;
+                case 10: //Code until a must-have break;
+                    Console.WriteLine("Number is 10"); 
+                    break;
+                case 15: //Code until a must-have break;
+                    Console.WriteLine("Number is 15"); 
+                    break;
+                case 20: //Code until a must-have break;
+                    Console.WriteLine("Number is 20"); 
+                    break;
+                default: // Like else
+                    Console.WriteLine("Number is unrecognized!"); 
+                    break; 
+           }
+
 
         }
 
