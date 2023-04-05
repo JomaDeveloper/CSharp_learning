2c93bdd527ca3ddd6d0a88afcd495ae32ca3feb9
Kirill <ktutaev@tickeron.com>
2023-04-05 16:36:54 +0300

    [Learning] for, while, do while

--- Program.cs
+++ Program.cs
@@ -5,33 +5,40 @@ namespace csharp_learning{
     class Program{
         
         static void Main() {
-           // Switch-case like conditional operator.
-           // But we can only check equality of the variable.
-           // But it can me useful when we expect a lot of inputs that needs variable checking.
-           // We can easily do it via if-else.
-           short user_input = Convert.ToInt16(Console.ReadLine());
 
-           switch(user_input) {
-                case 5: //Code until a must-have break;
-                    Console.WriteLine("Number is 5"); 
-                    break;
-                case 10: //Code until a must-have break;
-                    Console.WriteLine("Number is 10"); 
-                    break;
-                case 15: //Code until a must-have break;
-                    Console.WriteLine("Number is 15"); 
-                    break;
-                case 20: //Code until a must-have break;
-                    Console.WriteLine("Number is 20"); 
-                    break;
-                default: // Like else
-                    Console.WriteLine("Number is unrecognized!"); 
-                    break; 
+            // for(float i = 200; i > 10; i /= 2){
+            //     Console.WriteLine("Element: {0}", i);
+            // }
+            
+            // int i = 0;
+            // while (i < 10){
+            //     Console.WriteLine("Element: {0}", i);
+            //     i++;
+            // }
+
+            // bool isHasCar = true;
+            // string end;
+            // while (isHasCar){
+            //     Console.WriteLine("+");
+            //     end = Console.ReadLine();
+            //     if (end == "end")
+            //         isHasCar = false;
+            //     else if (end == "p")
+            //         break;
+            // }
+
+
+            // Do-While. At first it launches Do then check condition in While.
+            // Do will process one time for sure.
+            byte i = 100;
+            do{
+              Console.WriteLine("Element {0}", i);  
+            } while(i < 10);
+           
+
            }
 
 
         }
 
     }
-
-}
