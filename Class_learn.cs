d74e44aedefe8e8495d1f3992870eebac35f02ab
Kirill <ktutaev@tickeron.com>
2023-04-20 04:36:57 +0300

    [Learning] Конструкторы классов + static (Модиф.)

--- Class_learn.cs
+++ Class_learn.cs
@@ -4,6 +4,14 @@ namespace csharp_learning{
 
     class Class_learn{
 
+        // Static у поля или у метода обозначает, что мы сразу имеем доступ через класс, а не объекты.
+        // То есть это никак не связано с объектами, сразу через класс.
+        // Например -> Class_learn.count = 5;
+        public static int count;
+        public static void Print(){
+            System.Console.WriteLine("Count is {0}", count);
+        }
+
         //Описываем чертеж, общую хааректристику для объектов.
 
         //Модификатор доступа -> public, protected, private etc.
@@ -12,6 +20,19 @@ namespace csharp_learning{
         // private - данное поле будет доступен исключительно в этом классе. 
         // private по большей части нужен из-за инкапсуляции, чтобы переменные были доступны только в классе.
 
+        // Конструктор. Действия при создании объекта. Название должно быть то же, что и название класса.
+        // Может создавать сколько угодно конструкторов.
+        public Class_learn(string _name, int _weight, byte[] _coordinates){
+            System.Console.WriteLine("Object has been created");
+            setValues(_name, _weight, _coordinates);
+            count++;
+        }
+        // Второй констуктор, который будет срабатывать, если не указывать параметры.
+        public Class_learn(){
+            count++;
+        }
+
+        // Переменные характеристик объектов класса.
         public string name;
         public int weight;
         public byte[] coordinates;
--- Program.cs
+++ Program.cs
@@ -11,7 +11,6 @@ namespace csharp_learning{
             bot.name = "Bot";
             bot.weight = 800;
             bot.coordinates = new byte [] {0, 0, 0};
-
             System.Console.WriteLine(bot.name + " Weight:" + bot.weight);
 
           // Создаем другой объект на основе того же самого класса.
@@ -20,12 +19,18 @@ namespace csharp_learning{
             killer.weight = 1000;
             killer.coordinates = new byte [] {0, 0, 10};
           
-
           // Можем вызывать методы класса.
           Class_learn bee = new Class_learn();
           bee.setValues("Bee", 50, new byte[] {0, 0, 0});
-          bot.printValue();
-          
+          bee.printValue();
+
+          //Объект класса, который будет создаваться на основе конструктора, который принимаем переменные.
+          Class_learn car = new Class_learn("Car", 10, new byte []{1, 1, 1});
+          car.printValue();
+
+          //Обращаемся к переменной static. Сразу через класс, а не его объекты.
+          Class_learn.count = 5;
+          Class_learn.Print();
        }
     }    
 }
